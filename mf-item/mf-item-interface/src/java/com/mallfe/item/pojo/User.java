26e25268e54ac27f0956dc5c60c6300b997362b5
yangbin-dl <yangbin-jl@163.com>
2019-07-05 10:49:52 +0800

    add user.class

---
+++ mf-item/mf-item-interface/src/java/com/mallfe/item/pojo/User.java
@@ -0,0 +1,25 @@
+package com.mallfe.item.pojo;
+
+import lombok.Data;
+import tk.mybatis.mapper.annotation.KeySql;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * 描述
+ *
+ * @author Yangbin
+ * @since 2019/07/05
+ */
+@Table(name="mf_user")
+@Data
+public class User {
+    @Id
+    @KeySql(useGeneratedKeys=true)
+    private Long id;
+    private String username;
+    private String password;
+    private String truename;
+    private int lx;
+}
