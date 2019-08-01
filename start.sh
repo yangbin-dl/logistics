96b24cac9c861a71b6547ee011ae9398bcdff580
drduan <drduan@vip.qq.com>
2019-08-01 20:41:48 +0800

    s

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
-#/bin/bash
-APPLICATION = "logistics"
-echo "Start Condition Experiment Time: ${APPLICATION}
+#! /bin/shell
+
+APPLICATION="logistics"
+echo "Start Condition Experiment Time: ${APPLICATION}"
 nohup java -jar -server  -Xmx100M  mf-gateway/target/mf-gateway-1.0-SNAPSHOT.jar  >/dev/null 2>&1 &
 nohup java -jar -server  -Xmx100M  mf-registry/target/mf-registry-1.0-SNAPSHOT.jar >/dev/null 2>&1 &
 nohup java -jar -server  -Xmx200M  mf-item/mf-item-service/target/mf-item-service-1.0-SNAPSHOT.jar    >/dev/null 2>&1 &
-echo "Start Condition Experiment Time: ${APPLICATION}
+echo "Start Condition Experiment Time: ${APPLICATION}"
