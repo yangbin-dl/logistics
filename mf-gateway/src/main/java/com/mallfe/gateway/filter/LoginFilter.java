c544e1ab261212701e6c059357f7b81153290060
drduan <drduan@vip.qq.com>
2019-08-08 21:13:58 +0800

    jwt cookie

--- mf-gateway/src/main/java/com/mallfe/gateway/filter/LoginFilter.java
+++ mf-gateway/src/main/java/com/mallfe/gateway/filter/LoginFilter.java
@@ -1,16 +1,17 @@
 package com.mallfe.gateway.filter;
 
-import com.mallfe.common.utils.CookieUtils;
 import com.mallfe.common.utils.JwtUtils;
 import com.mallfe.gateway.cart.config.FilterProperties;
 import com.mallfe.gateway.cart.config.JwtProperties;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,6 +22,7 @@ import java.util.List;
  * @date 2018/6/13
  */
 @Component
+@Slf4j
 @EnableConfigurationProperties(value = {JwtProperties.class, FilterProperties.class})
 public class LoginFilter extends ZuulFilter {
     private Logger logger = LoggerFactory.getLogger(LoginFilter.class);
@@ -65,14 +67,17 @@ public class LoginFilter extends ZuulFilter {
         //获取上下文
         RequestContext ctx = RequestContext.getCurrentContext();
         HttpServletRequest request = ctx.getRequest();
-        String token = CookieUtils.getCookieValue(request, jwtProperties.getCookieName());
-
+//        String token = CookieUtils.getCookieValue(request, jwtProperties.getCookieName());
+        String requestUrl = request.getServletPath();
+        String ipAddr = request.getRemoteAddr();
+        log.info(String.format("%s [%s] >>> %s",ipAddr, request.getMethod(), requestUrl));
+        String token = ctx.getRequest().getHeader("Authorization");
         try {
             JwtUtils.getInfoFromToken(token, jwtProperties.getPublicKey());
         } catch (Exception e) {
             logger.error("{}", e.getMessage());
             ctx.setSendZuulResponse(false);
-            ctx.setResponseStatusCode(403);
+            ctx.setResponseStatusCode(HttpStatus.FORBIDDEN.value());
 
         }
         return null;
