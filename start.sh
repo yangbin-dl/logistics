6e7c42fd6e6fcc1932ada9f9c07b50cf5de1a9fe
drduan <7875572+drduan@users.noreply.github.com>
2019-10-29 14:23:21 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,7 @@ APPLICATION="logistics"
 echo "Start Condition Experiment Time: ${APPLICATION}"
 nohup java -jar -server  -Xmx100M  mf-gateway/target/mf-gateway-1.0-SNAPSHOT.jar  >/dev/null 2>&1 &
 nohup java -jar -server  -Xmx100M  mf-registry/target/mf-registry-1.0-SNAPSHOT.jar >/dev/null 2>&1 &
-nohup java -jar -server  -Xmx200M  mf-item/mf-item-service/target/mf-item-service-1.0-SNAPSHOT.jar    >/dev/null 2>&1 &
+#nohup java -jar -server  -Xmx200M  mf-item/mf-item-service/target/mf-item-service-1.0-SNAPSHOT.jar    >/dev/null 2>&1 &
+nohup java  -jar   -Xms156m -Xmx156m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=test.hprof 
+    mf-item/mf-item-service/target/mf-item-service-1.0-SNAPSHOT.jar  >/dev/null 2>&1 &
 echo "Start Condition Experiment Time: ${APPLICATION}"
