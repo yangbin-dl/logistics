a77db74f02858b96213bc1f2dfd6f626083c70d6
drduan <7875572+drduan@users.noreply.github.com>
2020-07-13 18:02:04 +0800

    Update GlobalCorsConfig.java
    *a

--- mf-gateway/src/main/java/com/mallfe/gateway/config/GlobalCorsConfig.java
+++ mf-gateway/src/main/java/com/mallfe/gateway/config/GlobalCorsConfig.java
@@ -15,38 +15,14 @@ public class GlobalCorsConfig {
         //1.添加CORS配置信息
         CorsConfiguration config = new CorsConfiguration();
         //1) 允许的域,不要写*，否则cookie就无法使用了
-        config.addAllowedOrigin("http://www.mallfe.com:9001");
-        config.addAllowedOrigin("http://localhost:9002");
-        config.addAllowedOrigin("http://localhost:10010");
-        config.addAllowedOrigin("http://127.0.0.1:9002");
-        config.addAllowedOrigin("http://47.105.221.242:81");
-        config.addAllowedOrigin("http://172.31.53.68");
-        config.addAllowedOrigin("http://47.105.221.242");
-        config.addAllowedOrigin("http://172.17.196.112");
-        config.addAllowedOrigin("http://47.94.95.93");
-        config.addAllowedOrigin("http://47.105.221.242:9002");
-
+        config.addAllowedOrigin("*");
         //2) 是否发送Cookie信息
         config.setAllowCredentials(true);
         //3) 允许的请求方式
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("HEAD");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("DELETE");
-        config.addAllowedMethod("PATCH");
-
-
+        config.addAllowedMethod("*");
         // 4）允许的头信息
         config.addAllowedHeader("*");
-//
-//        config.addAllowedHeader("Access-Control-Allow-Origin", "*");
-//        config.addAllowedHeader("Access-Control-Allow-Methods", "*");
-//        config.addAllowedHeader("Access-Control-Allow-Headers", "*");
-//        config.addAllowedHeader("Access-Control-Request-Headers","*");
-//        config.addAllowedHeader("Access-Control-Allow-Headers", "content-type,x-auth-token");
-//        config.addAllowedHeader("Access-Control-Expose-Headers", "*");
+
         //2.添加映射路径，我们拦截一切请求
         UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
         configSource.registerCorsConfiguration("/**", config);
